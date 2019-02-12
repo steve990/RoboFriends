90db7a0b7999578c139b45bce5ebe4329972aaac
Steve <steve_990@hotmail.com>
2019-02-12 14:08:25 -0800

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,122 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Elmo Sale',
+        username: 'esale',
+        email: 'esale@robomail.com'
+    },
+    {
+        id: 2,
+        name: 'Heike Mcdaniels',
+        username: 'hmcdaniels',
+        email: 'hmcdaniels@robomail.com'
+    },
+    {
+        id: 3,
+        name: 'Bronwyn Tremble',
+        username: 'btremble',
+        email: 'btremble@robomail.com'
+    },
+    {
+        id: 4,
+        name: 'Sanjuana Naughton',
+        username: 'snaughton',
+        email: 'snaughton@robomail.com'
+    },
+    {
+        id: 5,
+        name: 'Malika Holley',
+        username: 'mholley',
+        email: 'mholley@robomail.com'
+    },
+    {
+        id: 6,
+        name: 'Will Zipp',
+        username: 'wzipp',
+        email: 'wzipp@robomail.com'
+    },
+    {
+        id: 7,
+        name: 'Glinda Bouyer',
+        username: 'gbouyer',
+        email: 'gbouyer@robomail.com'
+    },
+    {
+        id: 8,
+        name: 'Sabra Maguire',
+        username: 'smaguire',
+        email: 'smaguire@robomail.com'
+    },
+    {
+        id: 9,
+        name: 'Mathilde People',
+        username: 'mpeople',
+        email: 'mpeople@robomail.com'
+    },
+    {
+        id: 10,
+        name: 'Merrilee Cloe',
+        username: 'mcloe',
+        email: 'mcloe@robomail.com'
+    },
+    {
+        id: 11,
+        name: 'Julius Atchley',
+        username: 'jatchley',
+        email: 'jatchley@robomail.com'
+    },
+    {
+        id: 12,
+        name: 'Neta Forys',
+        username: 'nforys',
+        email: 'nforys@robomail.com'
+    },
+    {
+        id: 13,
+        name: 'Bell Daws',
+        username: 'bdaws',
+        email: 'bdaws@robomail.com'
+    },
+    {
+        id: 14,
+        name: 'Augustine Depasquale',
+        username: 'adepasquale',
+        email: 'adepasquale@robomail.com'
+    },
+    {
+        id: 15,
+        name: 'Lenard Billips',
+        username: 'lbillips',
+        email: 'lbillips@robomail.com'
+    },
+    {
+        id: 16,
+        name: 'Myrta Eklund',
+        username: 'meklund',
+        email: 'meklund@robomail.com'
+    },
+    {
+        id: 17,
+        name: 'Cassie Mcguigan',
+        username: 'cmcguigan',
+        email: 'cmcguigan@robomail.com'
+    },
+    {
+        id: 18,
+        name: 'Delaine Hostetler',
+        username: 'dhostetler',
+        email: 'dhostetler@robomail.com'
+    },
+    {
+        id: 19,
+        name: 'Columbus Ralls',
+        username: 'cralls',
+        email: 'cralls@robomail.com'
+    },
+    {
+        id: 20,
+        name: 'Dana Doom',
+        username: 'ddoom',
+        email: 'ddoom@robomail.com'
+    }
+]
